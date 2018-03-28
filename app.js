83bcc460e5142e0a260c600c88f17539eacacd8e
Fajardo <juan.fajardo@accenture.com>
2018-03-28 09:28:20 -0500

    flujo desde Luis

--- app.js
+++ app.js
@@ -74,24 +74,29 @@ dialog.matches('cancelar', [
 
 dialog.matches('FligthStatus', [
             (session, args, status) => {
-                /* let sNumber = builder.EntityRecognizer.findEntity(args.entities, 'builtin.number');
-                let route = builder.EntityRecognizer.findEntity(args.entities, 'route');
-                let sTo = builder.EntityRecognizer.findEntity(args.entities, 'Location::toLocation');
-                let sFrom = builder.EntityRecognizer.findEntity(args.entities, 'Location::fromLocation');
+                let sNumber = builder.EntityRecognizer.findEntity(args.entities, 'builtin.number');
+                let sFrom = builder.EntityRecognizer.findAllEntities(args.entities, 'Ciudades');
+                let sTo = builder.EntityRecognizer.findAllEntities(args.entities, 'Ciudades');
                 let sDate = builder.EntityRecognizer.findEntity(args.entities, 'builtin.datetimeV2.date');
-                let fstatus = flight.status; */
+                let fstatus = 'Schedule';
                 this.control = '';
-                this.sNumber = null;
-                this.sTo = 'MED';
-                this.sFrom = 'BOG';
-                this.sDate = null;
-                this.fstatus = 'Schedule';
+                if (sFrom.length > 0) this.sFrom = sFrom[0].resolution.values;
+                if (sTo.length > 0) this.sTo = sTo[1].resolution.values;
+                if (sDate) this.sDate = sDate.entity;
+
+                if (sNumber) {
+                    if (sNumber && sDate) {
+                        if (sNumber.entity.startIndex !== sDate.entity.startIndex) {
+                            this.sNumber = sNumber.entity;
+                        }
+                    } else this.sNumber = sNumber.entity;
+                }
 
                 if (this.sDate) {
                     if (this.sFrom && this.sTo) {
-                        session.send(`El estado del vuelo No. ${this.sNumber ?`${this.sNumber}`:`123`} de fecha ${this.sDate} con ruta  ${this.sFrom} -  ${this.sTo}, es: ${this.fstatus}`);
+                        session.send(`El estado del vuelo No. ${this.sNumber ?`${this.sNumber}`:`123`} de fecha ${this.sDate} con ruta ${this.sFrom} -  ${this.sTo}, es: ${this.fstatus}`);
             } else if (this.sNumber) {
-                session.send(`El estado del vuelo No. ${this.sNumber} de fecha ${this.sDate} con ruta ${this.sFrom ?`${this.sFrom}`:`BOG`} - ${this.sTo ?`${this.sTo}`:`MED`}, es: ${this.fstatus}`);
+                session.send(`El estado del vuelo No. ${this.sNumber} de fecha ${this.sDate} con ruta ${this.sFrom ?`${this.sFrom}`:`Lima`} - ${this.sTo ?`${this.sTo}`:`MED`}, es: ${this.fstatus}`);
             } else {
                 this.control = 'getNumOrRou';
                 builder.Prompts.text(session, "Por favor digite numero del vuelo o ruta");
@@ -110,7 +115,7 @@ dialog.matches('FligthStatus', [
 
         if (this.control == 'getDate') {
             session.dialogData.fligthDate = results.response;
-            session.send(`El estado del vuelo No. ${this.sNumber ?`${this.sNumber}`:`123`} de fecha ${session.dialogData.fligthDate} con ruta ${this.sFrom ?`${this.sFrom}`:`BOG`} - ${this.sTo ?`${this.sTo}`:`MED`}, es: ${this.fstatus}`);
+            session.send(`El estado del vuelo No. ${this.sNumber ?`${this.sNumber}`:`123`} de fecha ${session.dialogData.fligthDate} con ruta ${this.sFrom ?`${this.sFrom.entity}`:`BOG`} - ${this.sTo ?`${this.sTo}`:`MED`}, es: ${this.fstatus}`);
         } else if (this.control == 'getNumOrRou') {
             session.dialogData.fligthNumber = results.response;
             session.send(`El estado del vuelo No. ${session.dialogData.fligthNumber} de fecha ${this.sDate} con ruta ${this.sFrom ?`${this.sFrom}`:`BOG`} - ${this.sTo ?`${this.sTo}`:`MED`}, es: ${this.fstatus}`);
