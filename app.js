f1b5321a64f0134aff56bcacd75a8a21d1b62822
Fajardo <juan.fajardo@accenture.com>
2018-04-12 10:38:27 -0500

    se hacen cambios para pruebas

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-'use strict'
-
-// This loads the environment variables from the .env file
-//require('dotenv-extended').load();
+/*-----------------------------------------------------------------------------
+A simple Language Understanding (LUIS) bot for the Microsoft Bot Framework. 
+-----------------------------------------------------------------------------*/
 
 var restify = require('restify');
 var builder = require('botbuilder');
 //var botbuilder_azure = require("botbuilder-azure");
+var msg = require('./bot/msg.json');
+var byRoute = require('./servicios/flightStatusByRoute');
+var call;
+var fStatus = require('./bot/actions/fligthStatus.1.js');
+var onDefault = require('./bot/actions/default.js');
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-  
+
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
-    openIdMetadata: process.env.BotOpenIdMetadata
+    openIdMetadata: process.env.BotOpenIdMetadata 
 });
 
 // Listen for messages from users 
@@ -29,34 +33,87 @@ server.post('/api/messages', connector.listen());
 * For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
 * ---------------------------------------------------------------------------------------- */
 
-var model = 'https://eastus.api.cognitive.microsoft.com/luis/v2.0/apps/64f40c86-c107-4029-b252-d55babae9310?subscription-key=56501f9ed4df464aa7df835ae26c1d13&verbose=true&timezoneOffset=0&q=';
-
 /* var tableName = 'botdata';
 var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient); */
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
+// This default message handler is invoked if the user's utterance doesn't
+// match any intents handled by other dialogs.
 
-var recognizer = new builder.LuisRecognizer(model);
-var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+
+var bot = new builder.UniversalBot(connector, { persistConversationData: true });
 
 //bot.set('storage', tableStorage);
 
-bot.dialog('/', dialog);
+// Make sure you add code to validate these fields
+var luisAppId = process.env.LuisAppId;
+var luisAPIKey = process.env.LuisAPIKey;
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+
+const LuisModelUrl = 'https://eastus.api.cognitive.microsoft.com/luis/v2.0/apps/64f40c86-c107-4029-b252-d55babae9310?subscription-key=56501f9ed4df464aa7df835ae26c1d13&verbose=true&timezoneOffset=0&q=';
+
+// Create a recognizer that gets intents from LUIS, and add it to the bot
+var recognizer = new builder.LuisRecognizer(LuisModelUrl);
+bot.recognizer(recognizer);
+
+// Add a dialog for each intent that the LUIS app recognizes.
+// See https://docs.microsoft.com/en-us/bot-framework/nodejs/bot-builder-nodejs-recognize-intent-luis
+
+
+/* // Create a custom prompt
+var prompt = new builder.Prompt({ defaultRetryPrompt: "I'm sorry. I didn't recognize your search." })
+    .onRecognize(function (context, callback) {
+        // Call prompts recognizer
+        recognizer.recognize(context, function (err, result) {
+            // If the intent returned isn't the 'None' intent return it
+            // as the prompts response.
+            if (result && result.intent !== 'None') {
+                callback(null, result.score, result);
+            } else {
+                callback(null, 0.0);
+            }
+        });
+    });
+
+
+bot.dialog('myLuisPrompt', prompt);
+
+
+builder.Prompts.myLuisPrompt = function (session, prompt, options) {
+    var args = options || {};
+    args.prompt = prompt || options.prompt;
+    session.beginDialog('myLuisPrompt', args);
+} */
+
+
+bot.dialog('GreetingDialog',
+    (session, args) => {
+
+        console.log('Greeting origen ->',  session.conversationData.origen);
+        console.log('Greeting destino ->',  session.conversationData.destino);
+        console.log('Greeting args', args);
+
+
+        session.send('You reached the Greeting intent. You said \'%s\'.', session.message.text);
+        session.endDialog();
+
+    }
+).triggerAction({
+    matches: 'Greeting',
+    intentThreshold: 0.9
+})
 
 
 
-//=========================================================
-// Dialogos - utilizamos el mismo que short-app.js
-//=========================================================
+bot.dialog('FligthStatusDialog', fStatus).triggerAction({
+    matches: 'FligthStatus'
+})
 
 
-// Acá se manejan los intents
-dialog.matches('Greeting', [require('./bot/actions/greeting.js')]);
 
-dialog.matches('FligthStatus', require('./bot/actions/fligthStatus.js'));
 
-//Este es el Default, cuando LUIS no entendió la consulta.
-dialog.onDefault(builder.DialogAction.send(
-    "No entendí. podrias repetir la solicitud por favor?"));
+bot.dialog('onDefault', onDefault).triggerAction({
+    matches: 'None',
+    intentThreshold: 0.1
+})
