9b2a536fcd6f4e4e7c85de72b1d95f5e38019f18
DIR\juan.fajardo <juanfar.group@gmail.com>
2018-05-02 15:54:54 -0500

    link login a prod azure

--- bot/actions/login.js
+++ bot/actions/login.js
@@ -17,7 +17,7 @@ module.exports = [
         .attachments([ 
             new builder.SigninCard(session) 
                 .text("Por favor click en este link para consultar tus millas.") 
-                .button("Ingresar", 'http://localhost:3978') 
+                .button("Ingresar", 'http://avi-bot-arch.azurewebsites.net') 
         ]); 
         //session.send(msg);
         builder.Prompts.text(session, msg);
